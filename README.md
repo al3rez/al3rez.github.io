374920136096fc6967fa663a5129023606ee555c
Alireza Bashiri <al3rez@gmail.com>
2024-09-09 12:48:34 +0700

    update domain

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-The website at [`hog.sh`](https://hog.sh), powered by Astro.
+The website at [`al3rez.com`](https://al3rez.com), powered by Astro.
 
 - Install: `npm install`
 - Build: `npm run build`
--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hog.sh",
+  site: "https://al3rez.com",
   integrations: [tailwind(), sitemap()],
   trailingSlash: "never",
   markdown: {
